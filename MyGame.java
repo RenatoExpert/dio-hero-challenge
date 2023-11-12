e0816f5966d6c4b6e7c2369050a9ab0080a76e00
renatoexpert <renatorraraujo@gmail.com>
2023-11-12 12:45:33 -0300

    fix some syntax issues

--- MyGame.java
+++ MyGame.java
@@ -1,20 +1,7 @@
 import java.util.Scanner;
 
 public class MyGame {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-
-		System.out.print("Hero's name: ");
-		String name = scanner.nextLine();
-
-		System.out.print("Experience: ");
-		int experience = scanner.nextInt();
-		String level = get_level(experience);
-
-		String message = String.format("O Herói de nome %s está no nível de %s", name, level);
-		System.out.println(message);
-	}
-	private String get_level(int experience) {
+	public static String get_level(int experience) {
 		String level;
 		if(experience <= 1000) {
 			level = "Ferro";
@@ -22,7 +9,7 @@ public class MyGame {
 			level = "Bronze";
 		} else if(experience <= 5000) {
 			level = "Prata";
-		} else if(experiene <= 7000) {
+		} else if(experience <= 7000) {
 			level = "Ouro";
 		} else if(experience <= 8000) {
 			level = "Platina";
@@ -34,5 +21,18 @@ public class MyGame {
 			level = "Radiante";
 		}
 		return level;
+	}
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Hero's name: ");
+		String name = scanner.nextLine();
+
+		System.out.print("Experience: ");
+		int experience = scanner.nextInt();
+		String level = MyGame.get_level(experience);
+
+		String message = String.format("O Herói de nome %s está no nível de %s", name, level);
+		System.out.println(message);
 	}
 }
